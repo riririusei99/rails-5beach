ce4c7216c3763f8f7b69b682f65e210a5b1ecfa0
riririusei99 <riry1313@gmail.com>
2019-06-15 06:23:43 +0000

    modify dbfile & rake db:migrate

--- db/migrate/20190615060812_create_events.rb
+++ db/migrate/20190615060812_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :events do |t|
-
+      t.date        :date
+      t.string      :title
+      t.text        :text
+      t.integer     :owner_id
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_08_080407) do
+ActiveRecord::Schema.define(version: 2019_06_15_060812) do
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.date "date"
+    t.string "title"
+    t.text "text"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname"
