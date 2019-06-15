9f4ddfa5a480c075ec14200eeed8b86b2f4c9d02
riririusei99 <riry1313@gmail.com>
2019-06-15 07:28:35 +0000

    add view files

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,5 +7,11 @@ class EventsController < ApplicationController
   end
 
   def create
+    Event.create(create_params)
+  end
+  
+  private
+  def create_params
+    params.require(:event).permit(:title, :date, :text).merge(owner_id: current_user.id)
   end
 end
